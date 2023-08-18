610f762c3f32a954f6d63e1fcdec8ee6fba5d976
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2023-08-18 18:39:35 +0530

    Update vars.py

--- YasirRoBot/vars.py
+++ YasirRoBot/vars.py
@@ -17,7 +17,7 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv("SLEEP_THRESHOLD", "60"))
     WORKERS = int(getenv("WORKERS", "4"))
     BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1001853354560"))
-    PORT = int(getenv("PORT", 8008))
+    PORT = int(getenv("PORT", 8000))
     BIND_ADRESS = str(getenv("WEB_SERVER_BIND_ADDRESS", "178.128.207.223"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5791145987").split())
